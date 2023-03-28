4161404966ef2bcf59592ea7a02fe26ad9ce15aa
ilker m. sitki <ilkermeliksitki@proton.me>
2023-03-28 11:18:15 +0300

    print the output of conjugation part and colorize it

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from bs4 import BeautifulSoup
 
 from helper import *
 
+BLUE = "\033[1;34m"
+RED = "\033[1;31m"
+RESET = "\033[0m"
+
 parser = argparse.ArgumentParser()
 parser.add_argument("word", help="the word that you want to look for.")
 parser.add_argument(
@@ -40,12 +44,16 @@ if args.declension:
 elif args.conjugation:
     name, meaning = get_name_and_meanaing_of_verb_conj(soup)
     if name and meaning:
-        print(name)
+        print(RED + name + RESET)
         print(meaning)
+        print()
     else:
         sys.exit(4)
-    conj_df = get_conjugation(soup)
-    if conj_df:
-        print(conj_df)
+    conjugations_d = get_conjugation(soup)
+    if conjugations_d:
+        for tense in conjugations_d.keys():
+            print(BLUE + tense + RESET)
+            print(conjugations_d[tense])
+            print()
 else:
     sys.exit(5)

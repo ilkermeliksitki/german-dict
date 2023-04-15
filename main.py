90666ec6e033ef065a3de0b9983c7cc6ad7f4535
ilker m. sitki <ilkermeliksitki@proton.me>
2023-04-15 17:52:54 +0300

    align color constants
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from bs4 import BeautifulSoup
 
 from helper import *
 
-BLUE  = "\033[1;34m"
-RED   = "\033[1;31m"
-GREEN = "\033[1;32m"
+BLUE   = "\033[1;34m"
+RED    = "\033[1;31m"
+GREEN  = "\033[1;32m"
 ITALIC = "\033[3m"
-RESET = "\033[0m"
+RESET  = "\033[0m"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("word", help="the word that you want to look for.")

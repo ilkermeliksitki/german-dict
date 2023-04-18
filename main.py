adf70b0411958c4733c679239492eb9e1e3df54c
ilker m. sitki <ilkermeliksitki@proton.me>
2023-04-18 18:57:42 +0300

    add db, config, and init_db functionality
    1-) configuration file is activated and initialized
    
    2-) database checking functionality is added.
    
    On branch db-ai-support
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+from db import *
 from helper import *
+from config import *
+from init_db import initialize_database
 
 BLUE   = "\033[1;34m"
 RED    = "\033[1;31m"
@@ -14,6 +17,12 @@ GREEN  = "\033[1;32m"
 ITALIC = "\033[3m"
 RESET  = "\033[0m"
 
+# configuration
+if not is_database_initialized():
+    initialize_database()
+    set_database_initialized()
+
+# argument parser - cli tool
 parser = argparse.ArgumentParser()
 parser.add_argument("word", help="the word that you want to look for.")
 parser.add_argument("-p", "--pronunciation", help="gives the link for the pronunciation of the word.", action="store_true")
@@ -24,44 +33,51 @@ args = parser.parse_args()
 
 word = args.word.strip()
 
-if args.declension:
-    r = requests.get(f"https://www.verbformen.com/declension/nouns/{word}.htm")
-elif args.conjugation:
-    r = requests.get(f"https://www.verbformen.de/konjugation/{word}.htm")
-elif args.word:
+if not check_word_exists(word):
     r = requests.get(f"https://www.verbformen.com/?w={word}")
+    if r.status_code == 429:
+        sys.stderr.write("Too many requests, slow  down\n")
+        sys.exit(3)
+
+    # cook the soup
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # parse word descriptors
+    word, word_type, gender, regular, auxiliary, separable = parse_word_descriptors(soup)
+
+    # get definition, definition_id and type_id based on parsed descriptors
+    definition = parse_definition(soup)
+    definition_id = add_definition_to_database(definition)
+    type_id = get_type_id(word_type)
+    print(definition_id, type_id)
+    # add word to the database
+    gender_id = None
+    if word_type == 'verb':
+        print('conjugations is adding...')
+    elif word_type == 'noun':
+        gender_id = get_gender_id(gender)
+        print('declension is adding...')
+    elif word_type == 'adjective':
+        pass
+
+    add_word_to_database((word, gender_id, auxiliary, regular, separable, definition_id, type_id))
+    print(parse_word_descriptors(soup))
+    pass
+else:
+    print('print word from database')
+#print(parse_declension(soup))
+#print(parse_conjugation(soup))
+
+if args.declension:
+    pass
+elif args.conjugation:
+    pass
+elif args.word:
+    pass
 else:
     sys.exit(2)
 
-if r.status_code == 429:
-    sys.stderr.write("Too many requests, slow  down\n")
-    sys.exit(3)
 
-soup = BeautifulSoup(r.text, "html.parser")
 
-if args.declension:
-    print(BLUE + get_name_with_article_dec(soup) + RESET)
-    print(RED + get_declension_definition(soup) + RESET, end='\n\n')
-    print(get_declension(soup))
-elif args.conjugation:
-    name, meaning = get_name_and_meanaing_of_verb_conj(soup)
-    if name and meaning:
-        print(RED + name + RESET)
-        print(meaning)
-        print()
-    else:
-        sys.exit(4)
-    conjugations_d = get_conjugation(soup)
-    if conjugations_d:
-        for tense in conjugations_d.keys():
-            print(BLUE + tense + RESET)
-            print(conjugations_d[tense])
-            print()
-elif args.word:
-    name, definition, plural = get_name_and_meaning_general(soup)
-    print(BLUE + name + RESET)
-    print(ITALIC + GREEN + plural + RESET)
-    print(RED + definition + RESET)
 
-else:
-    sys.exit(5)
+

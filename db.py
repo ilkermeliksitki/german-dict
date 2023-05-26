4bbc3124c0b50ab9f39d14958ccc601e1ea0c411
ilker m. sitki <ilkermeliksitki@proton.me>
2023-05-26 14:49:00 +0300

    couple of function is added for conjugation
    1- get_word_id
    2- get_definition_id
    3- get_definition
    4- add_conjugation_to_db
    5- print_conjugation_of_verb and _pronoun_sort_key (helper)
    
    On branch db-ai-support
    Changes to be committed:
    	modified:   db.py

--- db.py
+++ db.py
@@ -3,6 +3,8 @@ from typing import Tuple
 from sqlite3 import Cursor
 from sqlite3 import Connection
 
+from escape_sequences import *
+
 def _open_database(path='dictionary.db') -> Tuple[Cursor, Connection]:
     conn = sqlite3.connect(path)
     curr = conn.cursor()
@@ -35,7 +37,6 @@ def add_definition_to_database(definition: str) -> int:
     curr.execute('SELECT id FROM definitions WHERE definition = ?', (definition,))
     result = curr.fetchone()
     _close_database(curr, conn)
-    print('def add successful')
     return result[0]
 
 def get_type_id(type_: str) -> int:
@@ -52,4 +53,66 @@ def get_gender_id(gender: str) -> int:
     print(result)
     _close_database(curr, conn)
     return result[0]
+
+def get_word_id(word):
+    curr, conn = _open_database()
+    curr.execute('SELECT id FROM words WHERE word = ?', (word,))
+    result = curr.fetchone()
+    _close_database(curr, conn)
+    return result[0]
+
+def get_definition_id(word):
+    curr, conn = _open_database()
+    curr.execute('SELECT definition_id FROM words WHERE word = ?', (word,))
+    result = curr.fetchone()
+    _close_database(curr, conn)
+    return result[0]
+
+def get_definition(definition_id):
+    curr, conn = _open_database()
+    curr.execute('SELECT definition FROM definitions WHERE id = ?', (definition_id,))
+    result = curr.fetchone()
+    _close_database(curr, conn)
+    return result[0]
+
+# for now, only simple tense is being added.
+def add_conjugation_to_db(conjugation_dict, word_id):
+    curr, conn = _open_database()
+    simple_tense_dict = conjugation_dict['simple']
+    for tense in simple_tense_dict.keys():
+        for pronoun in simple_tense_dict[tense].keys():
+            conjugation = simple_tense_dict[tense][pronoun]
+            #print(tense, pronoun, conjugation, 'is added')
+            curr.execute(
+                "INSERT INTO conjugations (tense, pronoun, conjugation, word_id, mood_id) VALUES (?, ?, ?, ?, ?)",
+                (tense, pronoun, conjugation, word_id, 1)
+            )
+    _close_database(curr, conn)
+
+def _pronoun_sort_key(item):
+    pronoun = item[1]
+    pronoun_order = ['ich', 'du', 'er', 'wir', 'ihr', 'sie', '0', '1']
+    return pronoun_order.index(pronoun)
+
+def print_conjugation_of_verb(word, tense_id):
+    curr, conn = _open_database()
+    word_id = get_word_id(word)
+    tense_dict = {
+        1: 'present', 2: 'imperfect', 3: 'imperative', 4: 'present subj.',
+        5: 'imperf. subj.', 6: 'infinitive', 7: 'participle'
+    }
+    curr.execute('SELECT tense, pronoun, conjugation FROM conjugations WHERE word_id = ? and tense = ?',
+        (word_id, tense_dict[tense_id])
+    )
+    conjugation_ls = curr.fetchall()
+    # sort the list according to german pronouns
+    conjugation_ls = sorted(conjugation_ls, key=_pronoun_sort_key)
+    for index, conjugation_tuple in enumerate(conjugation_ls):
+        if index == 0:
+            print(RED + conjugation_tuple[0] + RESET)
+        # if pronoun appears numeric, do not write down
+        if not conjugation_tuple[1].isnumeric():
+            print(f"{BLUE + conjugation_tuple[1] + RESET:20} ", end='')
+        print(f"{GREEN + conjugation_tuple[2] + RESET}")
+    _close_database(curr, conn)
 

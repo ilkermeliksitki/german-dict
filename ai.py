c75b86cc2aef60cd0d86f94f82b06fa4d900d180
ilker m. sitki <ilkermeliksitki@proton.me>
2023-05-26 17:38:56 +0300

    change function names and fix stdout
    On branch db-ai-support
    Changes to be committed:
    	modified:   ai.py

--- ai.py
+++ ai.py
@@ -1,17 +1,21 @@
 import os
 import re
+import sys
 import openai
 from typing import List, Tuple
 
+from escape_sequences import *
+
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 MODEL_ENGINE = "text-davinci-003"
+#MODEL_ENGINE = "text-davinci-002"
 TEMPERATURE = 1.150
 
-def get_response() -> str:
+def get_openai_response(word) -> str:
+    print(ITALIC + 'waiting for open ai to get example sentences\n' + RESET)
     openai.api_key = OPENAI_API_KEY
     model_engine = MODEL_ENGINE
 
-    word = input("Enter a german word: ")
     prompt = f"prompt 5 numbered list example sentences that is used in daily life consisting \
 of at least 5 words for the word {word} in German and translate those sentences \
 in English, here is the example: \n<german sentence> - <english sentence>"
@@ -28,7 +32,7 @@ in English, here is the example: \n<german sentence> - <english sentence>"
     #generated_text = response.choices[0].text.strip()
     return response
 
-def parse_response(response: str) -> List[Tuple[str, str]]:
+def parse_openai_response(response: str) -> List[Tuple[str, str]]:
     """
         Define a function that takes a string `r` and returns
         a list of example sentence tuples
@@ -39,7 +43,11 @@ def parse_response(response: str) -> List[Tuple[str, str]]:
     for example in text_ls:
         # Extract the example number, German sentence, and English 
         # translation from the match object.
-        parsed_example_tp = re.findall(r'(\d)\W+(.*)\s-\s(.*)', example.strip())[0]
+        try:
+            parsed_example_tp = re.findall(r'(\d)\W+(.*)\s-\s(.*)', example.strip())[0]
+        except:
+            sys.stderr.write('open ai response error, please repeat the action.')
+            sys.exit(4)
         # if lenght of the tuple is not 3, consider it is not parsed 
         # correctly, and skip.
         if len(parsed_example_tp) != 3:

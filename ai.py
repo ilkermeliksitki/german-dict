af6479bc309bb4775ec10645e8579c09b469f0c7
ilker m. sitki <ilkermeliksitki@proton.me>
2024-07-13 11:23:34 +0200

    migrate to new openai api
    Changes to be committed:
    	modified:   ai.py

--- ai.py
+++ ai.py
@@ -1,33 +1,35 @@
 import os
 import re
 import sys
-import openai
+from openai import OpenAI
+
 from typing import List, Tuple
 
 from escape_sequences import *
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-MODEL_ENGINE = "text-davinci-003"
-#MODEL_ENGINE = "text-davinci-002"
+MODEL_ENGINE = "gpt-3.5-turbo-0125"
 TEMPERATURE = 1.150
 
 def get_openai_response(word) -> str:
     #print(ITALIC + 'waiting for open ai to get example sentences\n' + RESET)
-    openai.api_key = OPENAI_API_KEY
     model_engine = MODEL_ENGINE
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
     prompt = f"prompt 5 numbered list example sentences that is used in daily life consisting \
 of at least 5 words for the word {word} in German and translate those sentences \
 in English, here is the example: \n<german sentence> - <english sentence>"
 
-    # generate text from the prompt using the GPT-3 model
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
+    response = client.chat.completions.create(
+        model=MODEL_ENGINE,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ],
         max_tokens=256,
-        n=1,
         temperature=TEMPERATURE
     )
+    print(response)
 
     #generated_text = response.choices[0].text.strip()
     return response
@@ -38,7 +40,7 @@ def parse_openai_response(response: str) -> List[Tuple[str, str]]:
         a list of example sentence tuples
     """
     parsed_examples = []
-    text = response["choices"][0]["text"].strip()
+    text = response.choices[0].message.content.strip()
     text_ls = text.split('\n')
     for example in text_ls:
         # Extract the example number, German sentence, and English 
